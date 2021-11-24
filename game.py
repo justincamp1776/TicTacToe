4d9863597198d2ea739d42191794c58ee067b4ea
justincamp1776 <justincamp1776@gmail.com>
2021-11-24 13:15:50 -0500

    Created a play_again function once the game is complete

--- game.py
+++ game.py
@@ -10,7 +10,6 @@ class Game:
         self.player1 = Player(input("Please Enter your name   "), "X")
         self.computer = Player("Computer", "O")
         self.keep_playing = True
-        
         self.game_intro()
 
  
@@ -29,48 +28,65 @@ class Game:
         while  self.keep_playing:
             print(f'{self.player1.name}, make your selection.')
             self.player1.handle_turn()
+            self.check_winner()
             print(f'{self.computer.name} chooses:')
             self.computer.handle_AI_turn()
-            if self.keep_playing == True:
-                self.end_game()
-                break
+            self.check_winner()
+            
         
 
     def check_winner(self):
-        if Player.board[0] =="X" & Player.board[1] =="X" & Player.board[2] =="X": 
+        if Player.board[0] =="X" and Player.board[1] =="X" and Player.board[2] =="X": 
             self.keep_playing = False
             self.end_game(self.player1.name)
-        if Player.board[0] =="O" & Player.board[1] =="O" & Player.board[2] =="O":
+        if Player.board[0] =="O" and Player.board[1] =="O" and Player.board[2] =="O":
             self.keep_playing = False
             self.end_game(self.computer.name)
-        if Player.board[3] =="X" & Player.board[4] =="X" & Player.board[5] =="X": 
+        if Player.board[3] =="X" and Player.board[4] =="X" and Player.board[5] =="X": 
             self.keep_playing = False
             self.end_game(self.player1.name)
-        if Player.board[3] =="O" & Player.board[4] =="O" & Player.board[5] =="O":
+        if Player.board[3] =="O" and Player.board[4] =="O" and Player.board[5] =="O":
             self.keep_playing = False
             self.end_game(self.computer.name)
-        if Player.board[6] =="X" & Player.board[7] =="X" & Player.board[8] =="X": 
+        if Player.board[6] =="X" and Player.board[7] =="X" and Player.board[8] =="X": 
             self.keep_playing = False
             self.end_game(self.player1.name)
-        if Player.board[6] =="O" & Player.board[7] =="O" & Player.board[8] =="O":
+        if Player.board[6] =="O" and Player.board[7] =="O" and Player.board[8] =="O":
             self.keep_playing = False
             self.end_game(self.computer.name)
-        if Player.board[0] =="X" & Player.board[4] =="X" & Player.board[8] =="X": 
+        if Player.board[0] =="X" and Player.board[4] =="X" and Player.board[8] =="X": 
             self.keep_playing = False
             self.end_game(self.player1.name)
-        if Player.board[0] =="O" & Player.board[4] =="O" & Player.board[8] =="O":
+        if Player.board[0] =="O" and Player.board[4] =="O" and Player.board[8] =="O":
             self.keep_playing = False
             self.end_game(self.computer.name)
-        if Player.board[2] =="X" & Player.board[4] =="X" & Player.board[6] =="X": 
+        if Player.board[2] =="X" and Player.board[4] =="X" and Player.board[6] =="X": 
             self.keep_playing = False
             self.end_game(self.player1.name)
-        if Player.board[2] =="O" & Player.board[4] =="O" & Player.board[6] =="X":
+        if Player.board[2] =="O" and Player.board[4] =="O" and Player.board[6] =="X":
              self.keep_playing = False
              self.end_game(self.computer.name)
         
       
+    def play_again(self):
+        
+        while self.is_valid:
+            play_again = input("Would you like to play again? Enter Yes or No")
+            if play_again.lower == "Yes".lower:
+                self.is_valid == True
+                Player.board = [
+                                "-", "-", "-",
+                                "-", "-", "-",
+                                "-", "-", "-"
+                                ]
+                self.play_game()
+                break
+            elif play_again.lower == "No".lower:
+                print("Thanks for playing!")
 
         
 
-    def end_game(self):
-        print("Thanks for playing. Rerun the program to play again.")
+    def end_game(self, player_name):
+        print(f'{player_name} wins!')
+        self.play_again()
+        
