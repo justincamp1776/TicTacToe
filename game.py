814c13796e2e894054f9d271e46a8e6be45b19a1
justincamp1776 <justincamp1776@gmail.com>
2021-11-25 14:22:50 -0500

    Adjusted play_game while loop

--- game.py
+++ game.py
@@ -10,11 +10,12 @@ class Game:
 
         self.player1 = Human(input("Please Enter your name   "))
         self.computer = Computer("Computer")
-        self.keep_playing = True
+        self.game_over= False
         self.game_intro()
 
  
     def game_intro(self):
+
         # displays rules and initial board
         self.display_rules()
         self.player1.display_board()
@@ -25,8 +26,9 @@ class Game:
 
 
     def play_game(self):
+
         # This function loops through the handle_turn fucntions and checks for a winner after each iteration. 
-        while  self.keep_playing:
+        while not self.game_over:
             print(f'{self.player1.name}, make your selection.')
             self.player1.handle_turn()
             self.check_winner()
@@ -35,6 +37,7 @@ class Game:
             self.check_winner()
           
     def check_winner(self):
+
         # This function calls the 7 functions below and checks for every pattern that could result in a win.
         self.check_first_row()
         self.check_second_row()
@@ -46,74 +49,82 @@ class Game:
 
         
     def check_first_row(self):
+
         if Player.board[0] =="X" and Player.board[1] =="X" and Player.board[2] =="X": 
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.player1.name)
         elif Player.board[0] =="O" and Player.board[1] =="O" and Player.board[2] =="O":
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.computer.name)
 
     def check_second_row(self):
+
         if Player.board[3] =="X" and Player.board[4] =="X" and Player.board[5] =="X": 
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.player1.name)
         elif Player.board[3] =="O" and Player.board[4] =="O" and Player.board[5] =="O":
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.computer.name)
 
     def check_third_row(self):
+
         if Player.board[6] =="X" and Player.board[7] =="X" and Player.board[8] =="X": 
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.player1.name)
         elif Player.board[6] =="O" and Player.board[7] =="O" and Player.board[8] =="O":
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.computer.name)
 
     def check_first_col(self):
+
         if Player.board[0] =="X" and Player.board[3] =="X" and Player.board[6] =="X": 
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.player1.name)
         elif Player.board[0] =="O" and Player.board[3] =="O" and Player.board[6] =="O":
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.computer.name)
 
     def check_second_col(self):
+
         if Player.board[1] =="X" and Player.board[4] =="X" and Player.board[7] =="X": 
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.player1.name)
         elif Player.board[1] =="O" and Player.board[4] =="O" and Player.board[7] =="O":
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.computer.name)
 
 
     def check_third_col(self):
+
         if Player.board[2] =="X" and Player.board[5] =="X" and Player.board[8] =="X": 
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.player1.name)
         elif Player.board[2] =="O" and Player.board[5] =="O" and Player.board[8] =="O":
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.computer.name)
 
     def check_diagnol(self):
+
         if Player.board[0] =="X" and Player.board[4] =="X" and Player.board[8] =="X": 
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.player1.name)
         elif Player.board[0] =="O" and Player.board[4] =="O" and Player.board[8] =="O":
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.computer.name)
         elif Player.board[2] =="X" and Player.board[4] =="X" and Player.board[6] =="X": 
-            self.keep_playing = False
+            self.game_over = True
             self.play_again(self.player1.name)
         elif Player.board[2] =="O" and Player.board[4] =="O" and Player.board[6] =="X":
-             self.keep_playing = False
+             self.game_over = True
              self.play_again(self.computer.name)
      
     def play_again(self, player_name):
+        
         valid = False
         print(f'{player_name} wins')
         user_input = input("Would you like A Rematch? Enter Yes or No    ")
         if user_input.lower() == "Yes".lower():
-            self.keep_playing = True
+            self.game_over = False
             valid == True
             Player.board = [
                             "-", "-", "-",
@@ -123,13 +134,12 @@ class Game:
             self.game_intro()
        
         elif user_input.lower() == "No".lower():
-            print("Thanks for playing!")
-            self.keep_playing = False
+            self.end_game()
         
 
         
 
     def end_game(self):
-        
-        self.play_again()
+        self.game_over = True
+        print("Game Over. Thanks for playing.")
         
--- player.py
+++ player.py
@@ -15,11 +15,14 @@ class Player():
         
         
     def display_board(self):
+
+        # This function displays each index of the list in 3 rows of 3.
         print(Player.board[0] , " | " + Player.board[1], " | " + Player.board[2])
         print(Player.board[3] , " | " + Player.board[4], " | " + Player.board[5])
         print(Player.board[6] , " | " + Player.board[7], " | " + Player.board[8])
 
     def handle_turn(self):
+        
         selection = input(" Grid Instructions: Top Row: 1 - 3 -- Second Row: 4 - 6 -- Third Row: 7-9  Choose an EMPTY position from 1-9   ")
         if Player.board[int(selection)- 1] != "-":
             print("That position is already taken. Please select and empty postion")
